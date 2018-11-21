9c2d729247892a66dda5585f83bfea2b7afb2336
Elton Lin <eltonlin1998@gmail.com>
2018-11-20 22:28:13 -0500

    Add GridList component

--- absar/src/App.vue
+++ absar/src/App.vue
@@ -2,8 +2,8 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>Abasar</span>
+        <!-- <span class="font-weight-light">MATERIAL DESIGN</span> -->
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
@@ -15,24 +15,27 @@
       </v-btn>
     </v-toolbar>
 
-    <v-content>
+    <!-- <v-content>
       <HelloWorld/>
-    </v-content>
+    </v-content> -->
+    <grid-list></grid-list>
   </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld'
+import GridList from './components/GridList'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
-    return {
-      //
-    }
-  }
+	name: 'App',
+	components: {
+		HelloWorld,
+		GridList
+	},
+	data() {
+		return {
+			//
+		}
+	}
 }
 </script>
